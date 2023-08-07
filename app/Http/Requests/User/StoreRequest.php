78ba3346d355b564015d66d4a69412310178b57c
Adriaan Mouton <arouxmouton@gmail.com>
2023-08-07 23:32:25 +0200

    AL-18: added user controller

---
+++ app/Http/Requests/User/StoreRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'             => 'required',
+            'role'             => 'required',
+            'email'            => 'required|unique:users,email',
+            'password'         => "regex:/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/",
+            'confirm_password' => 'same:password',
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function messages(): array
+    {
+        return [
+            'regex'    => "Password must contain one digit, one lowercase, one uppercase,
+                one special character, no spaces, and be between 8-16 characters.",
+            'same'     => "Passwords don't match",
+        ];
+    }
+}
---
+++ app/Http/Requests/User/UpdateRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'name'  => 'required',
+            'role'  => 'required',
+            'email' => 'required|unique:users,email,' . $this->get('user_id'),
+        ];
+
+        if (!empty($this->get('password')) || $this->get('confirm_password')) {
+            $rules['password'] = "regex:/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/";
+            $rules['confirm_password'] = "same:password";
+        }
+
+        return $rules;
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function messages(): array
+    {
+        return [
+            'regex' => "Password must contain one digit, one lowercase, one uppercase,
+                one special character, no spaces, and be between 8-16 characters.",
+            'same' => "Passwords don't match",
+        ];
+    }
+}
