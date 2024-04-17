a36e2d7590d86bf38d3ef0b241a9f708c9f8af3f
Adriaan Mouton <arouxmouton@gmail.com>
2024-04-17 14:54:37 +0200

    AL-95: updated ordering of export columns

--- resources/views/models/reporting/export.blade.php
+++ resources/views/models/reporting/export.blade.php
@@ -2,13 +2,14 @@
     <thead>
     <tr>
         <th>{{ __('Sales rep') }}</th>
-        <th>{{ __('Company') }}</th>
-        <th>{{ __('Contact') }}</th>
-        <th>{{ __('Contact email') }}</th>
-        <th>{{ __('Meeting title') }}</th>
         <th>{{ __('Date') }}</th>
         <th>{{ __('Start time') }}</th>
         <th>{{ __('End time') }}</th>
+        <th>{{ __('Meeting title') }}</th>
+        <th>{{ __('Company') }}</th>
+        <th>{{ __('Company Type') }}</th>
+        <th>{{ __('Contact') }}</th>
+        <th>{{ __('Contact email') }}</th>
         <th>{{ __('Location') }}</th>
         <th>{{ __('Report') }}</th>
         <th>{{ __('Cancelled') }}</th>
@@ -19,13 +20,14 @@
     @foreach($meetings as $meeting)
         <tr>
             <td>{{ $meeting->user->name }}</td>
-            <td>{{ $meeting->company()->name }}</td>
-            <td>{{ $meeting->contact->name }}</td>
-            <td>{{ $meeting->contact->email }}</td>
-            <td>{{ $meeting->title }}</td>
             <td>{{ $meeting->date }}</td>
             <td>{{ $meeting->start_time }}</td>
             <td>{{ $meeting->end_time }}</td>
+            <td>{{ $meeting->title }}</td>
+            <td>{{ $meeting->company()->name }}</td>
+            <td>{{ $meeting->company()->companyType->name }}</td>
+            <td>{{ $meeting->contact->name }}</td>
+            <td>{{ $meeting->contact->email }}</td>
             <td>{{ $meeting->location }}</td>
             <td>{{ $meeting->report }}</td>
             <td>{{ $meeting->cancelled_at ? __('Yes') : __('No') }}</td>
