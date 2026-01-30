604311192965656ade8f58ff649cef45d77a9a30
Adriaan Mouton <arouxmouton@gmail.com>
2026-01-30 18:03:46 +0000

    AL-117 Ran pint

--- app/Models/EmailCron.php
+++ app/Models/EmailCron.php
@@ -28,14 +28,14 @@ class EmailCron extends Model
         $filterUsed = json_decode($this->filter, true);
         $filterUsedString = '';
         foreach ($filterUsed as $key => $value) {
-            if($key == 'report_id') {
+            if ($key == 'report_id') {
                 continue;
             }
             $value = $this->getKeyRealValue($key, $value);
             if (is_array($value)) {
                 $value = implode(', ', $value);
             }
-            $filterUsedString .= Str::ucfirst(Str::replace("_", " ", $key)).': '.($value??'n/a').'<br>';
+            $filterUsedString .= Str::ucfirst(Str::replace("_", " ", $key)).': '.($value ?? 'n/a').'<br>';
         }
         return $filterUsedString;
     }
@@ -48,7 +48,7 @@ class EmailCron extends Model
     protected function getKeyRealValue($key, $value)
     {
         $return = $value;
-        if($key == 'users') {
+        if ($key == 'users') {
             $userNames = User::whereIn('id', $value)->get()->pluck('name')->toArray();
             $return = implode(', ', $userNames);
         } elseif ($key == 'company_types') {
--- app/Models/Report.php
+++ app/Models/Report.php
@@ -30,14 +30,14 @@ class Report extends Model
         $filterUsed = json_decode($this->filter, true);
         $filterUsedString = '';
         foreach ($filterUsed as $key => $value) {
-            if($key == 'report_id') {
+            if ($key == 'report_id') {
                 continue;
             }
             $value = $this->getKeyRealValue($key, $value);
             if (is_array($value)) {
                 $value = implode(', ', $value);
             }
-            $filterUsedString .= Str::ucfirst(Str::replace("_", " ", $key)).': '.($value??'n/a').'<br>';
+            $filterUsedString .= Str::ucfirst(Str::replace("_", " ", $key)).': '.($value ?? 'n/a').'<br>';
         }
         return $filterUsedString;
     }
@@ -45,7 +45,7 @@ class Report extends Model
     protected function getKeyRealValue($key, $value)
     {
         $return = $value;
-        if($key == 'users') {
+        if ($key == 'users') {
             $userNames = User::whereIn('id', $value)->get()->pluck('name')->toArray();
             $return = implode(', ', $userNames);
         } elseif ($key == 'company_types') {
